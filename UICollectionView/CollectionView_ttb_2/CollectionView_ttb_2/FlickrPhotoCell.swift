e01eeea4611062a4ed402d0c98e6cc7502b462ea
Congllspkt <thanhcongnguyen1995@gmail.com>
2017-04-26 16:23:01 +0700

    Add files via upload

---
+++ UICollectionView/CollectionView_ttb_2/CollectionView_ttb_2/FlickrPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrPhotoCell.swift
+//  CollectionView_ttb_2
+//
+//  Created by Cntt27 on 4/26/17.
+//  Copyright © 2017 admin25. All rights reserved.
+//
+
+import UIKit
+
+internal class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
